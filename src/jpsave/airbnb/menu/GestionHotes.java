5679c982c1c5de8bd2b25896b32b56120da2ac35
J-Phiz <jp.save@gmail.com>
2021-01-08 16:21:02 +0100

    GestionHotes has too many exceptions

--- src/jpsave/airbnb/menu/GestionHotes.java
+++ src/jpsave/airbnb/menu/GestionHotes.java
@@ -7,30 +7,25 @@ import java.util.InputMismatchException;
 public class GestionHotes {
 
     private static void ajouterHote() throws InputMismatchException, NumberFormatException {
-                System.out.println("----------------------------------------");
-                System.out.print("Saisir le nom de l'hôte : ");
-                String nom = Menu.scanner.next();
+        System.out.println("----------------------------------------");
+        System.out.print("Saisir le nom de l'hôte : ");
+        String nom = Menu.scanner.next();
 
-                System.out.print("Saisir le prénom de l'hôte : ");
-                String prenom = Menu.scanner.next();
+        System.out.print("Saisir le prénom de l'hôte : ");
+        String prenom = Menu.scanner.next();
 
-                System.out.print("Saisir l'âge de l'hôte : ");
-                try {
-                    int age = Menu.scanner.nextInt();
-                    if (age < 18)
-                        throw new NumberFormatException("L'hôte doit être majeur pour s'enregister !");
+        System.out.print("Saisir l'âge de l'hôte : ");
+        int age = Menu.scanner.nextInt();
+        if (age < 1)
+            throw new NumberFormatException();
 
-                    Hote hote = new Hote(prenom, nom, age);
-                    Menu.hotes.add(hote);
-                    hote.afficher();
-                    System.out.println(" a été ajouté comme hôte (id=" + (Menu.hotes.size() - 1) + ").");
-                } catch (InputMismatchException e) {
-                    Menu.scanner.next();
-                    throw new InputMismatchException("Nous n'avons pas compris votre saisie !");
-                }
+        Hote hote = new Hote(prenom, nom, age);
+        Menu.hotes.add(hote);
+        hote.afficher();
+        System.out.println(" a été ajouté comme hôte.");
     }
 
-    private static void supprimerHote() throws InputMismatchException, IndexOutOfBoundsException {
+    private static void supprimerHote() throws IndexOutOfBoundsException {
         System.out.println("----------------------------------------");
         System.out.print("Saisir le nom de l'hôte : ");
         String nom = Menu.scanner.next();
@@ -49,7 +44,7 @@ public class GestionHotes {
                 return;
             }
         }
-        throw new IndexOutOfBoundsException("Nous n'avons pas trouvé l'identifiant de l'hôte !");
+        throw new IndexOutOfBoundsException();
     }
 
     static void listerHotes() {
@@ -64,14 +59,17 @@ public class GestionHotes {
             if(valChoix == 1) {
                 try {
                     ajouterHote();
-                } catch (NumberFormatException | InputMismatchException e) {
-                    System.out.println(e.getMessage());
+                } catch (NumberFormatException e) {
+                    System.out.println("Votre age doit être > 1 !");
+                } catch (InputMismatchException e) {
+                    Menu.scanner.next();
+                    System.out.println("Nous n'avons pas compris votre saisie !");
                 }
             } else if (valChoix == 2) {
                 try {
                     supprimerHote();
-                } catch (IndexOutOfBoundsException | InputMismatchException e) {
-                    System.out.println(e.getMessage());
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("Nous n'avons pas trouvé l'identifiant de l'hôte !");
                 }
             } else {
                 break;
