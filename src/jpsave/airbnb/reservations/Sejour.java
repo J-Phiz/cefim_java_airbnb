3a76039ded4be57dd617f1d8ed5ce26abc9e00dc
J-Phiz <jp.save@gmail.com>
2021-02-03 10:18:50 +0100

    Protect Date attribute in Sejour

--- src/jpsave/airbnb/reservations/Sejour.java
+++ src/jpsave/airbnb/reservations/Sejour.java
@@ -12,13 +12,52 @@ public abstract class Sejour implements SejourInterface {
     protected int tarif;
 
     public Sejour(Date dateArrivee, int nbNuits, Logement logement, int nbVoyageurs) {
-        this.dateArrivee = dateArrivee;
+        this.dateArrivee = (Date)dateArrivee.clone();
         this.nbNuits = nbNuits;
         this.logement = logement;
         this.nbVoyageurs = nbVoyageurs;
         miseAJourDuTarif();
     }
 
+    public Date getDateArrivee() {
+        return (Date)dateArrivee.clone();
+    }
+
+    public void setDateArrivee(Date dateArrivee) {
+        this.dateArrivee = (Date)dateArrivee.clone();
+    }
+
+    public Logement getLogement() {
+        return logement;
+    }
+
+    public void setLogement(Logement logement) {
+        this.logement = logement;
+    }
+
+    public int getNbVoyageurs() {
+        return nbVoyageurs;
+    }
+
+    public void setNbVoyageurs(int nbVoyageurs) {
+        this.nbVoyageurs = nbVoyageurs;
+    }
+
+    public int getNbNuits() {
+        return nbNuits;
+    }
+
+    public void setNbNuits(int nbNuits) {
+        this.nbNuits = nbNuits;
+    }
+
+    public int getTarif() {
+        return tarif;
+    }
+
+    public void setTarif(int tarif) {
+        this.tarif = tarif;
+    }
     /* Même si cette fct est demandée par l'interface
      * comme la classe Sejour est abstraite
      * il suffit que les enfants définisse la fonction
